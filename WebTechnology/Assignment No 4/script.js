2fed587fe1dbd09c6a6b31c11ad52d3278a95347
Sanket <Sanketkatkade782a@gmail.com>
2023-03-19 22:06:07 +0530

    initial commit

---
+++ WebTechnology/Assignment No 4/script.js
@@ -0,0 +1,47 @@
+//put in full screen for better rendering ;)
+
+var liste = [];
+var allnumber = "";
+
+
+function addToList(a) {
+    if (a === '+' || a === '-' || a === '*' || a === '/') {
+        liste.push(' ' + a + ' ');
+    } else {
+        liste.push(a);
+    }
+    console.log(liste);
+    document.getElementById("result").innerHTML = liste.join('');
+}
+
+function secureEval(expression) {
+    const regex = /^[\d\s()+\-*/.,]+$/ ;
+    if (!regex.test(expression)) {
+        throw new Error('Entrée non valide');
+    }
+    return eval(expression);
+}
+
+function joinList() {
+    allnumber = liste.join('');
+    console.log(allnumber + "joinlist");
+}
+
+function calc(listenumber) {
+    joinList(listenumber);
+    console.log(secureEval(allnumber) + "calc");
+    document.getElementById("result").innerHTML = secureEval(allnumber);
+    document.getElementById("calcul").innerHTML = allnumber;
+    liste = [];
+}
+
+function del() {
+    liste.pop();
+    document.getElementById("result").innerHTML = liste.join('');
+}
+
+function reset() {
+    liste = [];
+    document.getElementById("result").innerHTML = 0;
+    document.getElementById("calcul").innerHTML = "";
+}
